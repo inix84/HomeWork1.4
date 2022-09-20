131d7850a65540430bb789cac10055adc06dbafb
inix84 <inix84@mail.ru>
2022-09-21 01:10:48 +0500

    1,4. ДЗ 3. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
         int savings=0;
         for (int i=1; i<13; i++) {
             savings+=29000;
-            System.out.println(i + " месяц, сумма накоплений равна " + savings + " рублей");
+            System.out.print(i + " месяц, сумма накоплений равна " + savings + " рублей, а ");
+            savings+=savings/100;
+            System.out.println (savings + " - сумма с процентом. ");
         }
 
 
