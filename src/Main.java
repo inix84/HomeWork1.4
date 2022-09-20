22729c4c92788062b9c6037a3a2988cb13ee7b2b
inix84 <inix84@mail.ru>
2022-09-21 00:14:36 +0500

    1,4. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i=1; i <=10; i++) {
+        for (int i=10; i >=1; i--) {
             System.out.print(i + " ");
         }
 
