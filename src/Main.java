611df1da46b9dd0d33d076bd8cabf31e34f04203
inix84 <inix84@mail.ru>
2022-09-21 19:25:11 +0500

    1,4. 2 урок. ДЗ 3. Задание 2/2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int comet=79;
-        int year100=100;
-        int year200=200;
-        int year2022=2022;
-        for (int i=1; i<=(year2022+year100);i++) {
-        if (i%comet==0 && i>(year2022-year200)) {
-            System.out.println(i) ;
-        }
+        int multiplication=2;
+              for (int i=1; i<=10;i++) {
+            int meaning=i*multiplication;
+        System.out.println(multiplication + "*" + i + "=" + meaning) ;
+
         }
 
     }
