98ecf856e499fb033a4da4bd6247a0dff7939819
inix84 <inix84@mail.ru>
2022-09-21 16:05:52 +0500

    1,4. 2 урок. ДЗ 2. Задание 3/4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int savings=15000;
-        int AmountLimit=12_000_000;
+        //int AmountLimit=12_000_000;
         int month=0;
         double percent= 1.07;
         //System.out.println("Процент " + percent);
-        while (savings<AmountLimit) {
+        while (month<=9*12) {
             month++;
             savings*=percent;
             if (month%6==0) {
@@ -13,9 +13,9 @@ public class Main {
             }
 
         }
-        System.out.print("Для накопления до нужной суммы, понадобиться " + month +" месяцев");
-        int month1=month/12;
-        double month2=month%12;
-        System.out.print(", что составит " + month1 +" лет и "+ (int) month2 +" месяца.");
+        //System.out.print("Для накопления до нужной суммы, понадобиться " + month +" месяцев");
+        //int month1=month/12;
+        //double month2=month%12;
+        //System.out.print(", что составит " + month1 +" лет и "+ (int) month2 +" месяца.");
     }
 }
