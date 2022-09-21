49664b7b40fb61c6ddaa948f281e5ac14b96f609
inix84 <inix84@mail.ru>
2022-09-21 16:38:39 +0500

    1,4. 2 урок. ДЗ 2. Задание 4/4 (без условия, с циклом while)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int savings=15000;
-        //int AmountLimit=12_000_000;
-        int month=0;
-        double percent= 1.07;
-        //System.out.println("Процент " + percent);
-        while (month<=9*12) {
-            month++;
-            savings*=percent;
-            if (month%6==0) {
-                System.out.println("Месяц " + month +", сумма накоплений = "+ savings+" рублей");
-            }
+        int friday=7;
+        int dayMonth=31;
+        while (friday<=dayMonth) {
+            System.out.println("Сегодня пятница, "+friday+"-е число. Необходимо подготовить отчет.");
+            friday+=7;
+
 
         }
-        //System.out.print("Для накопления до нужной суммы, понадобиться " + month +" месяцев");
-        //int month1=month/12;
-        //double month2=month%12;
-        //System.out.print(", что составит " + month1 +" лет и "+ (int) month2 +" месяца.");
+
     }
 }
